1c6c813c4cba360a260ab95e2c0e5b1c8248e12a
Manuel Cardosa <macardosa@outlook.com>
2026-04-25 02:40:03 +0200

    allow customization of players properties and enhanced UI

--- scripts.js
+++ scripts.js
@@ -84,18 +84,21 @@ const gameboard = (function () {
 })();
 
 // players
-function Player(name, symbol) {
+function Player(name, symbol, color) {
     this.name = name;
     this.symbol = symbol;
+    this.color = color;
     this.score = 0;
 }
 
+Player.prototype.increaseScore = function () {
+    this.score++;
+}
+
+const players = []; // array to store players
+
 // logic for game controller
 const ticTacToe = (() => {
-    const players = [
-        new Player("player1", "X"),
-        new Player("player2", "O"),
-    ];
     let currentPlayerId = 0;
     let nextPlayerId = 1;
     let playing = false;
@@ -104,6 +107,7 @@ const ticTacToe = (() => {
     const DOM = (function () {
         const board = document.querySelector(".game-board");
         const statusBox = document.querySelector(".status-box");
+        const scoreBoxes = document.querySelectorAll(".info > :last-child");
 
         function displayGrid() {
             board.style.display = "grid";
@@ -112,16 +116,20 @@ const ticTacToe = (() => {
                 const row = index % gameboard.getNumRows();
                 const col = Math.floor(index / gameboard.getNumRows());
                 child.textContent = gameboard.getValue(row, col);
+                child.classList.remove("filled");
             });
         }
 
         board.addEventListener("click", (e) => {
             const symbol = players[currentPlayerId].symbol;
+            const color = players[currentPlayerId].color;
             const cell = e.target;
             if (playing && cell.textContent === "") {
-                cell.textContent = symbol;
                 const [i, j] = cell.id.slice(-3).split("-").map(x => Number(x));
                 gameboard.setValue(i, j, symbol);
+                cell.textContent = symbol;
+                cell.style.color = color;
+                cell.classList.add("filled");
                 ticTacToe.play();
             }
         });
@@ -130,35 +138,54 @@ const ticTacToe = (() => {
             statusBox.textContent = message;
         }
 
+        function setNewScore(winnerIndex, newScore) {
+            scoreBoxes[winnerIndex].textContent = `Score: ${newScore}`;
+        }
+
         return {
             displayGrid,
             setDisplayMessage,
+            setNewScore,
         }
 
     })();
 
     function start() {
-        console.log("Welcome to Tic Tac Toe");
+        // choose player that starts randomly
         currentPlayerId = Math.floor(Math.random() * 2);
         nextPlayerId = (currentPlayerId === 0) ? 1 : 0;
-        playing = true;
+
+        playing = true; // start game execution
         gameboard.start();
+
         DOM.displayGrid();
-        DOM.setDisplayMessage(`Turn: ${players[currentPlayerId].name} (${players[currentPlayerId].symbol})`);
+        DOM.setDisplayMessage(`Turn: ${players[currentPlayerId].name}`);
     }
 
     function switchPlayers() {
         [currentPlayerId, nextPlayerId] = [nextPlayerId, currentPlayerId];
-        DOM.setDisplayMessage(`Turn: ${players[currentPlayerId].name} (${players[currentPlayerId].symbol})`);
+        DOM.setDisplayMessage(`Turn: ${players[currentPlayerId].name}`);
+    }
+
+    function findIndexWinner(symbol) {
+        return (players[0].symbol === symbol)
+            ? 0
+            : 1;
     }
 
     function play() {
         const { status, result } = gameboard.gameOver();
         if (status) {
-            console.log(gameboard.toString());
-            DOM.setDisplayMessage(`Game Over!
-                Winner is ${result}`);
-            playing = false;
+            if (result === "tie") {
+                DOM.setDisplayMessage(`This match is a tie!`);
+            } else {
+                const winnerIndex = findIndexWinner(result);
+                const winner = players[winnerIndex].name;
+                DOM.setDisplayMessage(`${winner} wins!`);
+                players[winnerIndex].increaseScore();
+                DOM.setNewScore(winnerIndex, players[winnerIndex].score);
+            }
+            playing = false; // stop game execution
             return;
         }
         switchPlayers();
@@ -176,3 +203,55 @@ document.querySelector(".play-btn")
     .addEventListener("click", () => {
         ticTacToe.start();
     });
+
+// logic to start game and get user info
+const startBtn = document.querySelector(".start-btn");
+const playersNamesForm = document.querySelector(".players-names");
+startBtn.addEventListener("click", () => {
+    playersNamesForm.style.display = "flex";
+    startBtn.style.display = "none";
+});
+
+const header = document.querySelector(".header");
+const startGameBtn = document.querySelector(".start-game-btn");
+startGameBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // get name of players
+    const player1Name = playersNamesForm.elements.name1.value;
+    const player2Name = playersNamesForm.elements.name2.value;
+
+    // assign shape and color of symbol randomly
+    const randomChoiceSymbol = Math.floor(Math.random() * 2);
+    const randomChoiceColor = Math.floor(Math.random() * 2);
+    if (randomChoiceSymbol === 0 && randomChoiceColor === 0) {
+        players.push(new Player(player1Name, "X", "black"));
+        players.push(new Player(player2Name, "O", "white"));
+    } else if (randomChoiceSymbol === 0 && randomChoiceColor === 1) {
+        players.push(new Player(player1Name, "X", "white"));
+        players.push(new Player(player2Name, "O", "black"));
+    } else if (randomChoiceSymbol === 1 && randomChoiceColor === 0) {
+        players.push(new Player(player1Name, "O", "black"));
+        players.push(new Player(player2Name, "X", "white"));
+    } else {
+        players.push(new Player(player1Name, "O", "white"));
+        players.push(new Player(player2Name, "X", "black"));
+    }
+
+    // render game layout
+    playersNamesForm.style.display = "none";
+    header.style.display = "flex";
+    const symbolBoxes = header.querySelectorAll(".symbol");
+    symbolBoxes[0].textContent = players[0].symbol;
+    symbolBoxes[0].style.color = players[0].color;
+    symbolBoxes[1].textContent = players[1].symbol;
+    symbolBoxes[1].style.color = players[1].color;
+
+    // add names of each player to the header
+    const nameBoxes = document.querySelectorAll(".info > :first-child");
+    nameBoxes[0].textContent = player1Name;
+    nameBoxes[1].textContent = player2Name;
+});
+
+// copyright notice
+document.getElementById("year").textContent = new Date().getFullYear();
